786debdf09ddb3465c6b11e0c3fcfbb60ab634da
1a35e1 <1a35e1@protonmail.com>
2024-10-04 19:39:13 +0100

    feat: Basic tests

--- apps/contracts/src/SignalsFactory.sol
+++ apps/contracts/src/SignalsFactory.sol
@@ -1,56 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import 'forge-std/console.sol';
 import 'lib/openzeppelin-contracts/contracts/access/Ownable.sol';
-import 'lib/openzeppelin-contracts/contracts/proxy/Clones.sol';
-
 import './Signals.sol';
 
 /// @title SignalsFactory
 /// @notice Factory contract to create instances of the Signals contract
-/// @dev Uses OpenZeppelin's Clones library to create minimal proxy contracts
 contract SignalsFactory is Ownable {
-  /// @notice Address of the Signals implementation contract
-  address public implementation;
-  /// @notice Address of the underlying token (ERC20 or ERC721)
-  address public underlyingToken;
-  /// @notice Flag indicating if the underlying token is ERC20
-  bool public isERC20;
+  error FactoryDeploymentFailed();
+  error InvalidOwnerAddress();
+
   /// @notice Event emitted when a new Signals contract is created
-  /// @param newSignals Address of the newly created Signals contract
-  /// @param owner Owner of the newly created Signals contract
   event SignalsCreated(address indexed newSignals, address indexed owner);
 
-  /// @notice Custom error for invalid implementation address
-  error InvalidImplementationAddress();
-  /// @notice Custom error for invalid token address
-  error InvalidTokenAddress();
-  /// @notice Custom error for invalid owner address
-  error InvalidOwnerAddress();
-  /// @notice Custom error for clone creation failure
-  error CloneCreationFailed();
-
   /// @notice Constructor to initialize the factory
-  /// @param _implementation Address of the Signals implementation contract
-  /// @param _underlyingToken Address of the underlying token (ERC20 or ERC721)
-  /// @param _isERC20 Boolean indicating if the underlying token is ERC20
-  constructor(address _implementation, address _underlyingToken, bool _isERC20) {
-    if (_implementation == address(0)) revert InvalidImplementationAddress();
-    if (_underlyingToken == address(0)) revert InvalidTokenAddress();
-    implementation = _implementation;
-    underlyingToken = _underlyingToken;
-    isERC20 = _isERC20;
-  }
+  constructor() {}
 
   /// @notice Creates a new Signals contract
+  ///
   /// @param owner_ Address of the owner of the new Signals contract
+  /// @param underlyingToken Address of the underlying token
   /// @param threshold Minimum tokens required to propose an initiative
   /// @param lockDurationCap Maximum lock duration allowed
   /// @param proposalCap Maximum number of proposals allowed
   /// @param decayCurveType Type of decay curve to be used
   /// @return Address of the newly created Signals contract
-  function createSignals(
+  function create(
     address owner_,
+    address underlyingToken,
     uint256 threshold,
     uint256 lockDurationCap,
     uint256 proposalCap,
@@ -58,28 +36,25 @@ contract SignalsFactory is Ownable {
   ) external onlyOwner returns (address) {
     if (owner_ == address(0)) revert InvalidOwnerAddress();
 
-    address clone = Clones.clone(implementation);
-    if (clone == address(0)) revert CloneCreationFailed();
-    Signals(clone).initialize(
+    // Create a new instance of the Signals contract
+    Signals newSignals = new Signals();
+
+    // Initialize the new Signals contract
+    newSignals.initialize(
       owner_,
+      underlyingToken,
       threshold,
       lockDurationCap,
       proposalCap,
-      decayCurveType,
-      underlyingToken,
-      isERC20
+      decayCurveType
     );
-    Ownable(clone).transferOwnership(owner_);
 
-    emit SignalsCreated(clone, owner_);
+    // revert if a new Signals contract is not created
+    if (address(newSignals) == address(0)) revert FactoryDeploymentFailed();
 
-    return clone;
-  }
+    // Emit an event for the creation of the new contract
+    emit SignalsCreated(address(newSignals), owner_);
 
-  /// @notice Sets a new implementation address
-  /// @param newImplementation Address of the new implementation contract
-  function setImplementation(address newImplementation) external onlyOwner {
-    if (newImplementation == address(0)) revert InvalidImplementationAddress();
-    implementation = newImplementation;
+    return address(newSignals);
   }
 }
--- apps/contracts/src/__tests__/SignalsFactory.t.sol
+++ apps/contracts/src/__tests__/SignalsFactory.t.sol
@@ -1,62 +1,58 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
+import 'forge-std/console.sol';
 import 'forge-std/StdUtils.sol';
 import 'forge-std/mocks/MockERC20.sol';
 import {SignalsFactory} from '../SignalsFactory.sol';
 import {Signals} from '../Signals.sol';
 
 contract SignalsFactoryTest is Test {
-  SignalsFactory signalsFactory;
-  Signals signalsImplementation;
+  SignalsFactory factory;
   MockERC20 mockToken;
-  address owner;
-  address addr1;
-  address addr2;
-  address addr3;
-  address addr4;
-  address addr5;
+
+  address deployer;
+
+  address alice;
+  address bob;
+  address charlie;
 
   function setUp() public {
-    owner = address(this);
-    addr1 = address(0x1234);
-    addr2 = address(0x2345);
-    addr3 = address(0x3456);
-    addr4 = address(0x4567);
-    addr5 = address(0x5678);
+    deployer = address(this);
+
+    alice = address(0x1234);
+    bob = address(0x2345);
+    charlie = address(0x3456);
+
+    // Log the test addresses
+    console.log('Owner:', deployer);
+    console.log('Alice:', alice);
 
     // Deploy MockERC20 token and mint 1 million tokens
     mockToken = new MockERC20();
     mockToken.initialize('MockToken', 'MTK', 18);
     uint256 initialSupply = 1_000_000 * 10 ** 18;
-    deal(address(mockToken), owner, initialSupply);
+    deal(address(mockToken), deployer, initialSupply);
 
     // Distribute tokens to test addresses
-    deal(address(mockToken), addr1, 200_000 * 10 ** 18);
-    deal(address(mockToken), addr2, 200_000 * 10 ** 18);
-    deal(address(mockToken), addr3, 200_000 * 10 ** 18);
-    deal(address(mockToken), addr4, 200_000 * 10 ** 18);
-    deal(address(mockToken), addr5, 200_000 * 10 ** 18);
-
-    // Deploy Signals implementation
-    signalsImplementation = new Signals();
+    deal(address(mockToken), alice, 200_000 * 10 ** 18);
+    deal(address(mockToken), bob, 200_000 * 10 ** 18);
+    deal(address(mockToken), charlie, 200_000 * 10 ** 18);
 
     // Deploy SignalsFactory with the Signals implementation
-    signalsFactory = new SignalsFactory(
-      address(signalsImplementation),
-      address(mockToken), // Use MockERC20 token address for testing
-      true // isERC20 is set to true for testing purposes
-    );
+    factory = new SignalsFactory();
   }
 
-  function testDeploySignalsContractWithCorrectParameters() public {
+  function testFactoryDeploymentWithCorrectParameters() public {
     // Ensure the caller is the owner
-    vm.prank(owner);
+    vm.prank(deployer);
 
     // Deploy a new Signals contract using the factory
-    address newSignalsAddress = signalsFactory.createSignals(
-      addr1,
+    address newSignalsAddress = factory.create(
+      alice,
+      address(mockToken),
       100, // threshold
       12, // lockDurationCap
       5, // proposalCap
@@ -66,43 +62,31 @@ contract SignalsFactoryTest is Test {
     // Check that the Signals contract was deployed
     assertTrue(newSignalsAddress != address(0));
 
-    // Get the deployed Signals contract instance
-    Signals newSignals = Signals(newSignalsAddress);
+    console.log('[signals:instance]', newSignalsAddress);
+
+    // Load the Signals contract instance
+    Signals instance = Signals(newSignalsAddress);
 
     // Verify the parameters were initialized correctly
-    assertEq(newSignals.threshold(), 100);
-    assertEq(newSignals.lockDurationCap(), 12);
-    assertEq(newSignals.proposalCap(), 5);
-    assertEq(newSignals.decayCurveType(), 1);
-    assertEq(newSignals.underlyingToken(), address(mockToken));
-    assertTrue(newSignals.isERC20());
+    // assertEq(newSignals.threshold(), 100);
+    assertEq(instance.owner(), address(alice));
+    assertEq(instance.lockDurationCap(), 12);
+    assertEq(instance.proposalCap(), 5);
+    assertEq(instance.decayCurveType(), 1);
+    assertEq(instance.underlyingToken(), address(mockToken));
   }
 
-  function testRevertIfInvalidOwnerAddress() public {
-    // Attempt to deploy with an invalid owner address (address(0))
-    vm.prank(owner);
-    vm.expectRevert(bytes('Invalid owner address'));
-    signalsFactory.createSignals(
-      address(0), // Invalid owner address
-      100, // threshold
-      12, // lockDurationCap
-      5, // proposalCap
-      1 // decayCurveType
-    );
-  }
-
-  function testRevertIfCloneCreationFails() public {
+  function testRevertCreatingInstancesWithAnInvalidOwner() public {
     // Set the implementation to an invalid address and attempt to create a clone
-    vm.prank(owner);
-    signalsFactory.setImplementation(address(0));
-    vm.expectRevert(bytes('Clone creation failed'));
-    vm.prank(owner);
-    signalsFactory.createSignals(
-      addr1,
-      100, // threshold
-      12, // lockDurationCap
-      5, // proposalCap
-      1 // decayCurveType
+    vm.prank(deployer);
+    vm.expectRevert(abi.encodeWithSelector(SignalsFactory.InvalidOwnerAddress.selector));
+    factory.create(
+      address(0), // --- invalid owner address
+      address(mockToken),
+      100,
+      12,
+      5,
+      1
     );
   }
 }
