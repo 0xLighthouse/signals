7a63055a4abcbdba083e5ed41fe2f2671807d847
x43n <x43n@pm.me>
2024-10-06 13:53:20 +1100

    feat: Configure Tailwind (remove Tamagui)

--- apps/interface/src/app/page.tsx
+++ apps/interface/src/app/page.tsx
@@ -1,26 +1,14 @@
 'use client'
 
-import styles from './page.module.css'
 import { useState } from 'react'
-import { TamaguiProvider, Stack, Text, createTamagui } from '@tamagui/core'
-import { config } from '@tamagui/config/v3'
-
-// you usually export this from a tamagui.config.ts file
-const tamaguiConfig = createTamagui(config)
-
-// TypeScript types across all Tamagui APIs
-type Conf = typeof tamaguiConfig
-declare module '@tamagui/core' {
-  interface TamaguiCustomConfig extends Conf {}
-}
 
 export default function Home() {
-  const addSupport = (id) => {
+  const addSupport = (id: number) => {
     // Add logic for adding support to the idea
     console.log(`Support added to card with id: ${id}`)
   }
 
-  const removeSupport = (id) => {
+  const removeSupport = (id: number) => {
     // Add logic for removing support from the idea
     console.log(`Support removed from card with id: ${id}`)
   }
@@ -58,37 +46,31 @@ export default function Home() {
   ])
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <TamaguiProvider config={tamaguiConfig}>
-          <Stack gap>
-            {cards.map((card) => (
-              <Stack key={card.id} backgroundColor="lightgray" padding="$4" borderRadius="$4" gap>
-                <Stack>
-                  <Text fontWeight="bold" fontSize="$4">
-                    {card.title}
-                  </Text>
-                  <Text>{card.status}</Text>
-                </Stack>
-                <Text>
-                  {card.body.length > 100 ? `${card.body.substring(0, 100)}...` : card.body}
-                </Text>
-                <Stack direction="row" justifyContent="space-between">
-                  <Stack direction="row" gap>
-                    <button type={'button'} onClick={() => addSupport(card.id)}>Add Support</button>
-                    <button type={'button'} onClick={() => removeSupport(card.id)}>Remove Support</button>
-                  </Stack>
-                  <Stack alignItems="flex-end">
-                    <Text>#{card.rank}</Text>
-                    <Text>{card.tokenAmount}</Text>
-                    <Text>{card.tokenPercentage} of supply</Text>
-                  </Stack>
-                </Stack>
-              </Stack>
-            ))}
-          </Stack>
-        </TamaguiProvider>
-      </main>
+    <div className="flex flex-col gap-4">
+      {cards.map((card) => (
+        <div key={card.id} className="bg-gray-200 p-4 rounded-md flex flex-col gap-4">
+          <div className="flex flex-col">
+            <span className="font-bold text-xl">{card.title}</span>
+            <span>{card.status}</span>
+          </div>
+          <p>{card.body.length > 100 ? `${card.body.substring(0, 100)}...` : card.body}</p>
+          <div className="flex justify-between">
+            <div className="flex gap-4">
+              <button type={'button'} onClick={() => addSupport(card.id)}>
+                Add Support
+              </button>
+              <button type={'button'} onClick={() => removeSupport(card.id)}>
+                Remove Support
+              </button>
+            </div>
+            <div className="flex flex-col items-end">
+              <span>#{card.rank}</span>
+              <span>{card.tokenAmount}</span>
+              <span>{card.tokenPercentage} of supply</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
