5055cffdd5a8883c09a0a641c16f84ab807df40e
x43n <x43n@pm.me>
2024-10-08 19:56:19 +1100

    feat: WIP query balance from blockchain

--- apps/interface/src/components/containers/faucet-card.tsx
+++ apps/interface/src/components/containers/faucet-card.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import { useAccount } from 'wagmi'
+import { createPublicClient, erc20Abi, http } from 'viem'
+import { hardhat } from 'viem/chains'
 
-interface Props {}
+const TOKEN_CONTRACT_ADDRESS = '0x34A1D3fff3958843C43aD80F30b94c510645C316' // Replace with your token contract address
+// const TOKEN_ABI = [
+//   // Replace with your token's ABI
+//   'function balanceOf(address owner) view returns (uint256)',
+// ]
 
-export const FaucetCard: React.FC<Props> = () => {
+export const FaucetCard = () => {
+  const { address, connector } = useAccount()
+  const [balance, setBalance] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!address) return
+
+    const fetchBalance = async () => {
+      try {
+        if (!connector) return
+
+        const client = createPublicClient({
+          chain: hardhat,
+          transport: http('https://6391-157-211-27-89.ngrok-free.app'), // Use your provider URL
+        })
+
+        const balance = await client.readContract({
+          address: TOKEN_CONTRACT_ADDRESS,
+          abi: erc20Abi,
+          functionName: 'balanceOf',
+          args: [address],
+        })
+
+        console.log('Balance:', balance)
+      } catch (error) {
+        console.error('Error fetching balance:', error)
+      }
+    }
+
+    fetchBalance()
+  }, [address, connector])
+
   return (
     <Card>
       <CardHeader>
