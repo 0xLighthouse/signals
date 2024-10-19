02743b09a1faa61db5a155a1460a4f9cac35acd8
1a35e1 <1a35e1@protonmail.com>
2024-10-19 10:48:32 +0100

    chore: Organise structure

--- apps/contracts/src/Signals.sol
+++ apps/contracts/src/Signals.sol
@@ -68,12 +68,47 @@ import './DecayCurves.sol';
  * @author Patakk (Artist) <https://patakk.tumblr.com/>
  */
 contract Signals is Ownable, ReentrancyGuard {
+  /**
+   * @notice Represents an initiative in the Signals contract
+   * @dev Stores all relevant information about a single initiative
+   * 
+   * @param title The title of the initiative
+   * @param body The detailed body of the initiative in markdown format
+   * @param state The current state of the initiative
+   * @param proposer The address of the account that proposed this initiative
+   * @param timestamp The timestamp when the initiative was created
+   * @param lastActivity Used to determine if an initiative has become inactive and can be expired
+   */
+  struct Initiative {
+    string title;
+    string body;
+    InitiativeState state;
+    address proposer;
+    uint256 timestamp;
+    uint256 lastActivity;
+  }
+
   /// @notice Possible initiative states
   enum InitiativeState {
     Proposed,
     Accepted,
     Cancelled,
     Expired
+  }
+
+  /**
+   * @notice Struct to store lock information for each lockup
+   * 
+   * @param tokenAmount Amount of tokens locked
+   * @param lockDuration Total duration of the lock in intervals
+   * @param created Timestamp of when the lock was created
+   * @param withdrawn Flag indicating whether the locked tokens have been withdrawn
+   */
+  struct LockInfo {
+    uint256 tokenAmount;
+    uint256 lockDuration;
+    uint256 created;
+    bool withdrawn;
   }
 
   /// @notice Custom errors
@@ -96,7 +131,6 @@ contract Signals is Ownable, ReentrancyGuard {
   /// @notice Maximum number of proposals allowed
   uint256 public proposalCap;
 
-
   /**
    * @notice Interval used for lockup duration and calculating the decay curve
    * 
@@ -116,41 +150,6 @@ contract Signals is Ownable, ReentrancyGuard {
 
   /// @notice Inactivity threshold after which an initiative can be expired (in seconds)
   uint256 public activityTimeout = 60 days;
-
-  /**
-   * @notice Represents an initiative in the Signals contract
-   * @dev Stores all relevant information about a single initiative
-   * 
-   * @param title The title of the initiative
-   * @param body The detailed body of the initiative in markdown format
-   * @param state The current state of the initiative
-   * @param proposer The address of the account that proposed this initiative
-   * @param timestamp The timestamp when the initiative was created
-   * @param lastActivity Used to determine if an initiative has become inactive and can be expired
-   */
-  struct Initiative {
-    string title;
-    string body;
-    InitiativeState state;
-    address proposer;
-    uint256 timestamp;
-    uint256 lastActivity;
-  }
-
-  /**
-   * @notice Struct to store lock information for each lockup
-   * 
-   * @param tokenAmount Amount of tokens locked
-   * @param lockDuration Total duration of the lock in intervals
-   * @param created Timestamp of when the lock was created
-   * @param withdrawn Flag indicating whether the locked tokens have been withdrawn
-   */
-  struct LockInfo {
-    uint256 tokenAmount;
-    uint256 lockDuration;
-    uint256 created;
-    bool withdrawn;
-  }
 
   /// @notice (initiativeId => Initiative)
   mapping(uint256 => Initiative) public initiatives;
@@ -201,29 +200,6 @@ contract Signals is Ownable, ReentrancyGuard {
   /// @notice Event emitted when a supporter withdraws their tokens
   event TokensWithdrawn(uint256 indexed initiativeId, address indexed supporter, uint256 amount);
 
-  function initialize(
-    address owner_,
-    address _underlyingToken,
-    uint256 _proposalThreshold,
-    uint256 _acceptanceThreshold,
-    uint256 _maxLockIntervals,
-    uint256 _proposalCap,
-    uint256 _lockInterval,
-    uint256 _decayCurveType,
-    uint256[] calldata _decayCurveParameters
-  ) external isNotInitialized {
-    underlyingToken = _underlyingToken;
-    proposalThreshold = _proposalThreshold;
-    acceptanceThreshold = _acceptanceThreshold;
-    maxLockIntervals = _maxLockIntervals;
-    proposalCap = _proposalCap;
-    lockInterval = _lockInterval;
-    decayCurveType = _decayCurveType;
-    decayCurveParameters = _decayCurveParameters;
-
-    transferOwnership(owner_);
-  }
-
   /// @notice Do we event need this? It would revert if the initiativeId is out of bounds
   modifier exists(uint256 initiativeId) {
     if (initiativeId >= count) revert InitiativeNotFound();
@@ -246,29 +222,7 @@ contract Signals is Ownable, ReentrancyGuard {
     _;
   }
 
-  /**
-   * @notice Allows the owner to update the inactivity threshold
-   * 
-   * @param _newThreshold New inactivity threshold in seconds
-   */
-  function setInactivityThreshold(uint256 _newThreshold) external onlyOwner {
-    activityTimeout = _newThreshold;
-  }
-
-  /**
-   * @notice Proposes a new initiative
-   * 
-   * @param title Title of the initiative
-   * @param body Body of the initiative
-   */
-  function proposeInitiative(
-    string memory title,
-    string memory body
-  ) external hasSufficientTokens hasValidInput(title, body) {
-    _addInitiative(title, body);
-  }
-
-  function _addInitiative (
+  function _addInitiative(
     string memory title,
     string memory body
   ) internal hasValidInput(title, body) returns (uint256 id) {
@@ -287,35 +241,6 @@ contract Signals is Ownable, ReentrancyGuard {
 
     emit InitiativeProposed(initiativeId, msg.sender, title, body);
     return initiativeId;
-  }
-
-  /**
-   * @notice Proposes a new initiative with locked tokens
-   * 
-   * @param title Title of the initiative
-   * @param body Body of the initiative
-   * @param amount Amount of tokens to lock
-   * @param duration Duration for which tokens are locked (in intervals)
-   */
-  function proposeInitiativeWithLock(
-    string memory title,
-    string memory body,
-    uint256 amount,
-    uint256 duration
-  ) external hasSufficientTokens hasValidInput(title, body) {
-    uint256 id = _addInitiative(title, body);
-    _addLock(id, msg.sender, amount, duration);
-  }
-
-  /**
-   * @notice Allows a user to support an existing initiative with locked tokens
-   * 
-   * @param initiativeId ID of the initiative to support
-   * @param amount Amount of tokens to lock
-   * @param intervals Duration for which tokens are locked (in months)
-   */
-  function supportInitiative(uint256 initiativeId, uint256 amount, uint256 intervals) external exists(initiativeId) {
-    _addLock(initiativeId, msg.sender, amount, intervals);
   }
 
   function _addLock(
@@ -363,46 +288,6 @@ contract Signals is Ownable, ReentrancyGuard {
     );
   }
 
-  function acceptInitiative(uint256 initiativeId) exists(initiativeId) external onlyOwner payable {
-    Initiative storage initiative = initiatives[initiativeId];
-    if (initiative.state != InitiativeState.Proposed)
-      revert InvalidInitiativeState('Initiative is not in Proposed state');
-
-    initiative.state = InitiativeState.Accepted;
-
-    emit InitiativeAccepted(initiativeId);
-  }
-
-  function expireInitiative(uint256 initiativeId) exists(initiativeId) external onlyOwner payable {
-    Initiative storage initiative = initiatives[initiativeId];
-    if (initiative.state != InitiativeState.Proposed)
-      revert InvalidInitiativeState('Initiative is not in Proposed state');
-    if (block.timestamp <= initiative.lastActivity + activityTimeout)
-      revert InvalidInitiativeState('Initiative not yet eligible for expiration');
-
-    initiative.state = InitiativeState.Expired;
-
-    emit InitiativeExpired(initiativeId);
-  }
-
-  function withdrawTokens(uint256 initiativeId) exists(initiativeId) public nonReentrant {
-    Initiative storage initiative = initiatives[initiativeId];
-    if (initiative.state != InitiativeState.Accepted && initiative.state != InitiativeState.Expired)
-      revert InvalidInitiativeState('Initiative not in a withdrawable state');
-
-    LockInfo[] storage _locks = locks[initiativeId][msg.sender];
-    uint256 withdrawAmount = _markWithdrawnFromLocks(_locks);
-
-    if (withdrawAmount == 0) revert NothingToWithdraw();
-
-    _removePendingWithdrawal(msg.sender, initiativeId);
-
-    if (!IERC20(underlyingToken).transfer(msg.sender, withdrawAmount))
-      revert TokenTransferFailed();
-
-    emit TokensWithdrawn(initiativeId, msg.sender, withdrawAmount);
-  }
-
   function _markWithdrawnFromLocks(
     LockInfo[] storage _locks
   ) internal returns (uint256 totalAmount) {
@@ -414,42 +299,6 @@ contract Signals is Ownable, ReentrancyGuard {
       }
     }
     return total;
-  }
-
-  // TODO: Redo withdrawal tracking, then simplify iterating over pending withdrawals
-  function withdrawAllTokens() external nonReentrant {
-    uint256[] storage initiativesToWithdraw = pendingWithdrawals[msg.sender];
-    uint256 totalInitiatives = initiativesToWithdraw.length;
-
-    uint256 totalToWithdraw = 0;
-    uint256 i = 0;
-    while (i < totalInitiatives) {
-      uint256 initiativeId = initiativesToWithdraw[i];
-      Initiative storage initiative = initiatives[initiativeId];
-      LockInfo[] storage _locks = locks[initiativeId][msg.sender];
-
-      if (
-        initiative.state == InitiativeState.Accepted ||
-        initiative.state == InitiativeState.Expired
-      ) {
-        uint256 amountToWithdraw = _markWithdrawnFromLocks(_locks);
-        totalToWithdraw += amountToWithdraw;
-
-        _removePendingWithdrawal(msg.sender, initiativeId);
-        totalInitiatives--;
-
-        if (!IERC20(underlyingToken).transfer(msg.sender, amountToWithdraw))
-          revert TokenTransferFailed();
-
-        emit TokensWithdrawn(initiativeId, msg.sender, amountToWithdraw);
-      } else {
-        i++;
-      }
-    }
-
-    if (totalToWithdraw == 0) {
-      revert NothingToWithdraw();
-    }
   }
 
   //TODO: Redo withdrawal tracking
@@ -474,42 +323,6 @@ contract Signals is Ownable, ReentrancyGuard {
     }
     pendingWithdrawals[supporter].pop();
     delete _pendingWithdrawalIndex[supporter][initiativeId];
-  }
-
-  function getInitiative(uint256 initiativeId) external view exists(initiativeId) returns (Initiative memory) {
-    return initiatives[initiativeId];
-  }
-
-  function balanceOf(address account) public view returns (uint256) {
-    return IERC20(underlyingToken).balanceOf(account);
-  }
-
-  function getSupporters(uint256 initiativeId) external view exists(initiativeId) returns (address[] memory) {
-    return supporters[initiativeId];
-  }
-
-  function getWeight(uint256 initiativeId) external view exists(initiativeId) returns (uint256) {
-    uint256 totalCurrentWeight = 0;
-    address[] memory _supporters = supporters[initiativeId];
-    
-    for (uint256 i = 0; i < _supporters.length; i++) {
-      address supporter = _supporters[i];
-      LockInfo[] storage lockInfos = locks[initiativeId][supporter];
-      uint256 lockCount = lockInfos.length;
-      for (uint256 j = 0; j < lockCount; j++) {
-        uint256 currentWeight = _calculateLockWeightAt(lockInfos[j], block.timestamp);
-        totalCurrentWeight += currentWeight;
-      }
-    }
-
-    return totalCurrentWeight;
-  }
-
-  function getWeightAt(
-    uint256 initiativeId,
-    uint256 timestamp
-  ) external view exists(initiativeId) returns (uint256) {
-    return _calculateWeightAt(initiativeId, timestamp);
   }
 
   function _calculateWeightAt(
@@ -556,6 +369,195 @@ contract Signals is Ownable, ReentrancyGuard {
     } else {
       return DecayCurves.exponential(lock.lockDuration, lock.tokenAmount, elapsedIntervals, decayCurveParameters);
     }
+  }
+
+  /**
+   * @notice Permit initializing the contract exactly once
+   */
+  function initialize(
+    address owner_,
+    address _underlyingToken,
+    uint256 _proposalThreshold,
+    uint256 _acceptanceThreshold,
+    uint256 _maxLockIntervals,
+    uint256 _proposalCap,
+    uint256 _lockInterval,
+    uint256 _decayCurveType,
+    uint256[] calldata _decayCurveParameters
+  ) external isNotInitialized {
+    underlyingToken = _underlyingToken;
+    proposalThreshold = _proposalThreshold;
+    acceptanceThreshold = _acceptanceThreshold;
+    maxLockIntervals = _maxLockIntervals;
+    proposalCap = _proposalCap;
+    lockInterval = _lockInterval;
+    decayCurveType = _decayCurveType;
+    decayCurveParameters = _decayCurveParameters;
+
+    transferOwnership(owner_);
+  }
+
+  /**
+   * @notice Proposes a new initiative
+   * 
+   * @param title Title of the initiative
+   * @param body Body of the initiative
+   */
+  function proposeInitiative(
+    string memory title,
+    string memory body
+  ) external hasSufficientTokens hasValidInput(title, body) {
+    _addInitiative(title, body);
+  }
+
+  /**
+   * @notice Proposes a new initiative with locked tokens
+   * 
+   * @param title Title of the initiative
+   * @param body Body of the initiative
+   * @param amount Amount of tokens to lock
+   * @param duration Duration for which tokens are locked (in intervals)
+   */
+  function proposeInitiativeWithLock(
+    string memory title,
+    string memory body,
+    uint256 amount,
+    uint256 duration
+  ) external hasSufficientTokens hasValidInput(title, body) {
+    uint256 id = _addInitiative(title, body);
+    _addLock(id, msg.sender, amount, duration);
+  }
+
+  /**
+   * @notice Allows a user to support an existing initiative with locked tokens
+   * 
+   * @param initiativeId ID of the initiative to support
+   * @param amount Amount of tokens to lock
+   * @param intervals Duration for which tokens are locked (in months)
+   */
+  function supportInitiative(uint256 initiativeId, uint256 amount, uint256 intervals) external exists(initiativeId) {
+    _addLock(initiativeId, msg.sender, amount, intervals);
+  }  
+
+  function acceptInitiative(uint256 initiativeId) exists(initiativeId) external onlyOwner payable {
+    Initiative storage initiative = initiatives[initiativeId];
+    if (initiative.state != InitiativeState.Proposed)
+      revert InvalidInitiativeState('Initiative is not in Proposed state');
+
+    initiative.state = InitiativeState.Accepted;
+
+    emit InitiativeAccepted(initiativeId);
+  }
+
+  function expireInitiative(uint256 initiativeId) exists(initiativeId) external onlyOwner payable {
+    Initiative storage initiative = initiatives[initiativeId];
+    if (initiative.state != InitiativeState.Proposed)
+      revert InvalidInitiativeState('Initiative is not in Proposed state');
+    if (block.timestamp <= initiative.lastActivity + activityTimeout)
+      revert InvalidInitiativeState('Initiative not yet eligible for expiration');
+
+    initiative.state = InitiativeState.Expired;
+
+    emit InitiativeExpired(initiativeId);
+  }
+
+  function withdrawTokens(uint256 initiativeId) exists(initiativeId) public nonReentrant {
+    Initiative storage initiative = initiatives[initiativeId];
+    if (initiative.state != InitiativeState.Accepted && initiative.state != InitiativeState.Expired)
+      revert InvalidInitiativeState('Initiative not in a withdrawable state');
+
+    LockInfo[] storage _locks = locks[initiativeId][msg.sender];
+    uint256 withdrawAmount = _markWithdrawnFromLocks(_locks);
+
+    if (withdrawAmount == 0) revert NothingToWithdraw();
+
+    _removePendingWithdrawal(msg.sender, initiativeId);
+
+    if (!IERC20(underlyingToken).transfer(msg.sender, withdrawAmount))
+      revert TokenTransferFailed();
+
+    emit TokensWithdrawn(initiativeId, msg.sender, withdrawAmount);
+  }
+
+  // TODO: Redo withdrawal tracking, then simplify iterating over pending withdrawals
+  function withdrawAllTokens() external nonReentrant {
+    uint256[] storage initiativesToWithdraw = pendingWithdrawals[msg.sender];
+    uint256 totalInitiatives = initiativesToWithdraw.length;
+
+    uint256 totalToWithdraw = 0;
+    uint256 i = 0;
+    while (i < totalInitiatives) {
+      uint256 initiativeId = initiativesToWithdraw[i];
+      Initiative storage initiative = initiatives[initiativeId];
+      LockInfo[] storage _locks = locks[initiativeId][msg.sender];
+
+      if (
+        initiative.state == InitiativeState.Accepted ||
+        initiative.state == InitiativeState.Expired
+      ) {
+        uint256 amountToWithdraw = _markWithdrawnFromLocks(_locks);
+        totalToWithdraw += amountToWithdraw;
+
+        _removePendingWithdrawal(msg.sender, initiativeId);
+        totalInitiatives--;
+
+        if (!IERC20(underlyingToken).transfer(msg.sender, amountToWithdraw))
+          revert TokenTransferFailed();
+
+        emit TokensWithdrawn(initiativeId, msg.sender, amountToWithdraw);
+      } else {
+        i++;
+      }
+    }
+
+    if (totalToWithdraw == 0) {
+      revert NothingToWithdraw();
+    }
+  }
+
+  /**
+   * @notice Allows the owner to update the inactivity threshold
+   * 
+   * @param _newThreshold New inactivity threshold in seconds
+   */
+  function setInactivityThreshold(uint256 _newThreshold) external onlyOwner {
+    activityTimeout = _newThreshold;
+  }
+
+  function getInitiative(uint256 initiativeId) external view exists(initiativeId) returns (Initiative memory) {
+    return initiatives[initiativeId];
+  }
+
+  function balanceOf(address account) public view returns (uint256) {
+    return IERC20(underlyingToken).balanceOf(account);
+  }
+
+  function getSupporters(uint256 initiativeId) external view exists(initiativeId) returns (address[] memory) {
+    return supporters[initiativeId];
+  }
+
+  function getWeight(uint256 initiativeId) external view exists(initiativeId) returns (uint256) {
+    uint256 totalCurrentWeight = 0;
+    address[] memory _supporters = supporters[initiativeId];
+    
+    for (uint256 i = 0; i < _supporters.length; i++) {
+      address supporter = _supporters[i];
+      LockInfo[] storage lockInfos = locks[initiativeId][supporter];
+      uint256 lockCount = lockInfos.length;
+      for (uint256 j = 0; j < lockCount; j++) {
+        uint256 currentWeight = _calculateLockWeightAt(lockInfos[j], block.timestamp);
+        totalCurrentWeight += currentWeight;
+      }
+    }
+
+    return totalCurrentWeight;
+  }
+
+  function getWeightAt(
+    uint256 initiativeId,
+    uint256 timestamp
+  ) external view exists(initiativeId) returns (uint256) {
+    return _calculateWeightAt(initiativeId, timestamp);
   }
 
   function getWeightForSupporterAt(
