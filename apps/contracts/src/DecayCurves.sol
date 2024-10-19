8e948861b5a4a8c48e20c9e5a5295e5c93fd5eae
1a35e1 <1a35e1@protonmail.com>
2024-10-19 09:04:38 +0100

    chore: Formatting

--- apps/contracts/src/DecayCurves.sol
+++ apps/contracts/src/DecayCurves.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.8.0;
 import 'forge-std/console.sol';
 
 library DecayCurves {
-  /// @notice Linear decay curve
-  /// @param lockDuration Total number of intervals of lock
-  /// @param lockAmount Amount of tokens locked
-  /// @param currentInterval How many intervals have passed
-  /// @param curveParameters Linear takes just one value. e.g. 1e18 = 1:1 linear decay
-  /// @return The weight of the lock at the current interval
 
-  function linear(uint256 lockDuration, uint256 lockAmount, uint256 currentInterval, uint256[] memory curveParameters) internal pure returns (uint256) {
+  /**
+  * @notice Linear decay curve
+  * @param lockDuration Total number of intervals of lock
+  * @param lockAmount Amount of tokens locked
+  * @param currentInterval How many intervals have passed
+  * @param curveParameters Linear takes just one value. e.g. 1e18 = 1:1 linear decay
+  * @return The weight of the lock at the current interval
+  */
+  function linear(
+    uint256 lockDuration,
+    uint256 lockAmount,
+    uint256 currentInterval,
+    uint256[] memory curveParameters
+  ) internal pure returns (uint256) {
 
     require(curveParameters.length == 1, 'Invalid curve parameters');
     require(currentInterval <= lockDuration, 'Invalid interval');
@@ -24,16 +31,20 @@ library DecayCurves {
     return weight;
   }
 
-  /// @notice Exponential decay curve
-  /// @param lockDuration Total number of intervals of lock
-  /// @param lockAmount Amount of tokens locked
-  /// @param currentInterval How many intervals have passed
-  /// @param curveParameters This curve takes just one value. e.g. 9e17 = 0.9 (10% reduced each interval)
-  /// @return The weight of the lock at the current interval
-
-  function exponential(uint256 lockDuration, uint256 lockAmount, uint256 currentInterval, uint256[] memory curveParameters) internal pure returns (uint256) {
-
-
+  /**
+  * @notice Exponential decay curve
+  * @param lockDuration Total number of intervals of lock
+  * @param lockAmount Amount of tokens locked
+  * @param currentInterval How many intervals have passed
+  * @param curveParameters This curve takes just one value. e.g. 9e17 = 0.9 (10% reduced each interval)
+  * @return The weight of the lock at the current interval
+  */
+  function exponential(
+    uint256 lockDuration,
+    uint256 lockAmount,
+    uint256 currentInterval,
+    uint256[] memory curveParameters
+  ) internal pure returns (uint256) {
     require(curveParameters.length == 1, 'Invalid curve parameters');
     require(currentInterval <= lockDuration, 'Invalid interval');
 
@@ -50,17 +61,17 @@ library DecayCurves {
 
     return weight;
   }
+
+  //TODO: Revisit this for differential decay
+  /*
+    // Using a differential decay model instead of linear decay
+    // dW/dt = -k * W where k is a constant and W is the current weight
+    // W(t) = W0 * exp(-k * t)
+    // For simplicity, k is set to 1 / weightedDuration for an exponential decay over the duration
+  
+    uint256 decayFactor = exp((elapsedTime * 1e18) / lockInfo.weightedDuration);
+    uint256 currentWeight = (lockInfo.totalAmount * remainingDuration * decayFactor);
+
+    return currentWeight;
+  */
 }
-
-    //TODO: Revisit this for differential decay
-    /*
-      // Using a differential decay model instead of linear decay
-      // dW/dt = -k * W where k is a constant and W is the current weight
-      // W(t) = W0 * exp(-k * t)
-      // For simplicity, k is set to 1 / weightedDuration for an exponential decay over the duration
-    
-      uint256 decayFactor = exp((elapsedTime * 1e18) / lockInfo.weightedDuration);
-      uint256 currentWeight = (lockInfo.totalAmount * remainingDuration * decayFactor);
-
-      return currentWeight;
-    */
