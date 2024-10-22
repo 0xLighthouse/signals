99f3a388e48d4c05bedd922edfcb1f07de4b2d3e
James MacWhyte <5316685+jmacwhyte@users.noreply.github.com>
2024-10-22 11:18:39 +0100

    fix: Simplified weight categories to two (existing, new) regardless of threshold

--- apps/interface/src/lib/chart.ts
+++ apps/interface/src/lib/chart.ts
@@ -23,7 +23,7 @@ export function generateTicks(
   newLock: Lock[] = []
 ): ChartTick[] {
 
-  const startTime: number = DateTime.now().toUnixInteger() - chartInterval * 2
+  const startTime: number = DateTime.now().toUnixInteger() - chartInterval * 2 // We give a small buffer to the start time
   const endTime: number = Math.max(
     getDefaultEnd(existingData, initiative.lockInterval),
     getDefaultEnd(newLock, initiative.lockInterval),
@@ -37,62 +37,74 @@ export function generateTicks(
   )
 
   const ticks: ChartTick[] = []
-
-  let hasExistingThreshold = false
-  let hasInputBase = false
-  let hasInputThreshold = false
-
-
   for (let i = 0; i < normalisedExistingData.length; i++) {
-    const existingWeight = normalisedExistingData[i].y
-    const inputWeight = normalisedInputData[i].y
-
     const tick: ChartTick = {
       label: normalisedExistingData[i].label,
-      existingBase: 0,
+      existingBase: normalisedExistingData[i].weight,
     }
-
-    // If the existing weight is above the acceptance threshold, we need to split it
-    if (existingWeight > acceptanceThreshold) {
-      tick.existingBase = acceptanceThreshold
-      tick.existingThreshold = existingWeight - acceptanceThreshold
-      hasExistingThreshold = true
-
-      // If there is input weight, it must all be above the threshold
-      if (inputWeight > 0) {
-        tick.inputThreshold = inputWeight
-        hasInputThreshold = true
-      }
-
-    } else {
-      // Otherwise, all existing weight is below the threshold
-      tick.existingBase = existingWeight
-
-      // If there is input weight, we need to split it
-      if (inputWeight > 0) {
-        const remainingThreshold = acceptanceThreshold - existingWeight
-
-        if (inputWeight > remainingThreshold) {
-          tick.inputBase = remainingThreshold
-          tick.inputThreshold = inputWeight - remainingThreshold
-          hasInputBase = true
-          hasInputThreshold = true
-        } else {
-          tick.inputBase = inputWeight
-          hasInputBase = true
-        }
-      }
+    if (newLock.length > 0) {
+      tick.inputBase = normalisedInputData[i].weight
     }
-
     ticks.push(tick)
   }
 
-  // Fill in zeros for all areas we have
-  for (let i = 0; i < ticks.length; i++) {
-    if (hasExistingThreshold) ticks[i].existingThreshold = ticks[i].existingThreshold || 0
-    if (hasInputBase) ticks[i].inputBase = ticks[i].inputBase || 0
-    if (hasInputThreshold) ticks[i].inputThreshold = ticks[i].inputThreshold || 0
-  }
+  // NOTE: The following is for segmenting portions above and below the threshold line into separate areas.
+  // The library we are using right now doesn't look very good that way, so we are using a more simple version for now but may return to this later.
+
+  // let hasExistingThreshold = false
+  // let hasInputBase = false
+  // let hasInputThreshold = false
+
+  // for (let i = 0; i < normalisedExistingData.length; i++) {
+  //   const existingWeight = normalisedExistingData[i].y
+  //   const inputWeight = normalisedInputData[i].y
+
+  //   const tick: ChartTick = {
+  //     label: normalisedExistingData[i].label,
+  //     existingBase: 0,
+  //   }
+
+  //   // If the existing weight is above the acceptance threshold, we need to split it
+  //   if (existingWeight > acceptanceThreshold) {
+  //     tick.existingBase = acceptanceThreshold
+  //     tick.existingThreshold = existingWeight - acceptanceThreshold
+  //     hasExistingThreshold = true
+
+  //     // If there is input weight, it must all be above the threshold
+  //     if (inputWeight > 0) {
+  //       tick.inputThreshold = inputWeight
+  //       hasInputThreshold = true
+  //     }
+
+  //   } else {
+  //     // Otherwise, all existing weight is below the threshold
+  //     tick.existingBase = existingWeight
+
+  //     // If there is input weight, we need to split it
+  //     if (inputWeight > 0) {
+  //       const remainingThreshold = acceptanceThreshold - existingWeight
+
+  //       if (inputWeight > remainingThreshold) {
+  //         tick.inputBase = remainingThreshold
+  //         tick.inputThreshold = inputWeight - remainingThreshold
+  //         hasInputBase = true
+  //         hasInputThreshold = true
+  //       } else {
+  //         tick.inputBase = inputWeight
+  //         hasInputBase = true
+  //       }
+  //     }
+  //   }
+
+  //   ticks.push(tick)
+  // }
+
+  // // Fill in zeros for all areas we have
+  // for (let i = 0; i < ticks.length; i++) {
+  //   if (hasExistingThreshold) ticks[i].existingThreshold = ticks[i].existingThreshold || 0
+  //   if (hasInputBase) ticks[i].inputBase = ticks[i].inputBase || 0
+  //   if (hasInputThreshold) ticks[i].inputThreshold = ticks[i].inputThreshold || 0
+  // }
 
   return ticks
 }
