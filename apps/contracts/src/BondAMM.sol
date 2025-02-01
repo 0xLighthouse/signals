62e8d74c25a60eed652b207a22285efd173a7c03
1a35e1 <1a35e1@protonmail.com>
2025-02-01 14:43:37 +0000

    fix: Compoile with Uniswap v4 compat

--- apps/contracts/src/BondAMM.sol
+++ apps/contracts/src/BondAMM.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.28;
+pragma solidity 0.8.26;
 
 import {BaseHook} from '@v4-periphery/base/hooks/BaseHook.sol';
 import {PoolKey} from '@v4-core/types/PoolKey.sol';
