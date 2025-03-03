6266ba17afaccb681e43396d11392192f7110e66
James MacWhyte <5316685+jmacwhyte@users.noreply.github.com>
2025-03-03 16:09:29 +0000

    fix: Remove unneeded function (and add interface files)

---
+++ apps/contracts/src/interfaces/IIncentives.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ISignals.sol";
+
+interface IIncentives {
+    enum Conditions {
+        NONE,
+        ACCEPTED_ON_OR_BEFORE_TIMESTAMP
+    }
+
+    struct Incentive {
+        uint256 initiativeId;
+        IERC20 token;
+        uint256 amount;
+        uint256 paid;
+        uint256 refunded;
+        uint256 expiresAt;
+        address contributor;
+        Conditions terms;
+    }
+
+    event IncentiveAdded(
+        uint256 indexed incentiveId,
+        uint256 indexed initiativeId,
+        address indexed token,
+        uint256 amount,
+        uint256 expiresAt,
+        Conditions terms
+    );
+
+    event IncentivePaidOut(
+        uint256 indexed incentiveId,
+        uint256 protocolAmount,
+        uint256 voterAmount,
+        uint256 treasuryAmount
+    );
+
+    event IncentivesUpdated(uint256 version);
+
+    event RewardClaimed(
+        uint256 indexed initiativeId,
+        address indexed supporter,
+        uint256 amount
+    );
+
+    event IncentiveRefunded(
+        uint256 indexed initiativeId,
+        address indexed contributor,
+        uint256 amount
+    );
+
+    function signalsContract() external view returns (ISignals);
+    function registry() external view returns (address);
+    function allocations(uint256 version) external view returns (uint256[3] memory);
+    function receivers(uint256 version) external view returns (address[3] memory);
+    function incentives(uint256 id) external view returns (Incentive memory);
+    function balances(address account, address token) external view returns (uint256);
+    function incentivesByInitiative(uint256 initiativeId) external view returns (uint256[] memory);
+    function version() external view returns (uint256);
+    function incentiveCount() external view returns (uint256);
+
+    function updateSplits(uint256[3] memory _allocations, address[3] memory _receivers) external;
+    function config(uint256 _version) external view returns (uint256, uint256[3] memory, address[3] memory);
+    function getIncentives(uint256 _initiativeId) external view returns (address[] memory, uint256[] memory, uint256);
+    function addIncentive(uint256 _initiativeId, address _token, uint256 _amount, uint256 _expiresAt, Conditions _terms) external payable;
+    function previewRewards(uint256 _initiativeId, uint256 _tokenId) external view returns (uint256);
+    function handleInitiativeAccepted(uint256 _initiativeId) external;
+    function handleInitiativeExpired(uint256 _initiativeId) external;
+}
---
+++ apps/contracts/src/interfaces/ISignals.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IBondIssuer} from "./IBondIssuer.sol";
+import {IIncentives} from "./IIncentives.sol";
+
+interface ISignals is IERC721Enumerable {
+    /**
+     * @notice All configuration parameters for initializing the Signals contract
+     *
+     * @param owner The address which will own the contract
+     * @param underlyingToken The address of the underlying ERC20 token
+     * @param proposalThreshold Minimum tokens to propose an initiative
+     * @param acceptanceThreshold Minimum tokens to accept an initiative
+     * @param maxLockIntervals Maximum lock intervals allowed
+     * @param proposalCap The maximum number of proposals
+     * @param lockInterval Time interval for lockup duration and decay calculations
+     * @param decayCurveType Which decay curve to use (e.g., 0 = linear, 1 = exponential)
+     * @param decayCurveParameters Parameters to control the decay curve behavior
+     */
+    struct SignalsConfig {
+        address owner;
+        address underlyingToken;
+        uint256 proposalThreshold;
+        uint256 acceptanceThreshold;
+        uint256 maxLockIntervals;
+        uint256 proposalCap;
+        uint256 lockInterval;
+        uint256 decayCurveType;
+        uint256[] decayCurveParameters;
+    }
+
+    /**
+     * @notice Represents an initiative in the Signals contract
+     * @dev Stores all relevant information about a single initiative
+     *
+     * @param title The title of the initiative
+     * @param body The detailed body of the initiative in markdown format
+     * @param state The current state of the initiative
+     * @param proposer The address of the account that proposed this initiative
+     * @param timestamp The timestamp when the initiative was created
+     * @param lastActivity Used to determine if an initiative has become inactive and can be expired
+     */
+    struct Initiative {
+        string title;
+        string body;
+        InitiativeState state;
+        address proposer;
+        uint256 timestamp;
+        uint256 lastActivity;
+        uint256 underlyingLocked;
+    }
+
+    /**
+     * @notice Details for each lockup
+     *
+     * @param initiativeId ID of the initiative
+     * @param tokenAmount Amount of tokens locked
+     * @param lockDuration Total duration of the lock in intervals
+     * @param created Timestamp of when the lock was created
+     * @param withdrawn Flag indicating whether the locked tokens have been withdrawn
+     */
+    struct TokenLock {
+        uint256 initiativeId;
+        uint256 tokenAmount;
+        uint256 lockDuration;
+        uint256 created;
+        bool withdrawn;
+    }
+
+    // Enums
+    enum InitiativeState {
+        Proposed,
+        Accepted,
+        Cancelled,
+        Expired
+    }
+
+    /**
+     * @notice Event emitted when a supporter supports an initiative
+     *
+     * @param initiativeId ID of the initiative
+     * @param supporter Address of the supporter
+     * @param tokenAmount Amount of tokens locked
+     * @param lockDuration Duration for which tokens are locked (in intervals)
+     * @param timestamp Timestamp of when the support was made
+     */
+    event InitiativeSupported(
+        uint256 indexed initiativeId,
+        address indexed supporter,
+        uint256 tokenAmount,
+        uint256 lockDuration,
+        uint256 timestamp
+    );
+    event InitiativeProposed(uint256 indexed initiativeId, address indexed proposer, string title, string body);
+    event InitiativeAccepted(uint256 indexed initiativeId, address indexed actor);
+    event InitiativeExpired(uint256 indexed initiativeId, address indexed actor);
+    event Redeemed(uint256 indexed tokenId, address indexed actor, uint256 amount);
+    event DecayCurveUpdated(uint256 decayCurveType, uint256[] decayCurveParameters);
+
+    // Errors
+    error InvalidInput(string message);
+    error InsufficientTokens();
+    error InvalidInitiativeState(string message);
+    error TokenTransferFailed();
+    error InvalidRedemption();
+    error InitiativeNotFound();
+    error InvalidTokenId();
+
+    // Public state variables
+    function proposalThreshold() external view returns (uint256);
+    function acceptanceThreshold() external view returns (uint256);
+    function maxLockIntervals() external view returns (uint256);
+    function proposalCap() external view returns (uint256);
+    function lockInterval() external view returns (uint256);
+    function decayCurveType() external view returns (uint256);
+    function decayCurveParameters(uint256) external view returns (uint256);
+    function underlyingToken() external view returns (address);
+    function activityTimeout() external view returns (uint256);
+    function initiatives(uint256) external view returns (Initiative memory);
+    function locks(uint256) external view returns (TokenLock memory);
+    function initiativeLocks(uint256, uint256) external view returns (uint256);
+    function supporterLocks(address, uint256) external view returns (uint256);
+    function supporters(uint256, uint256) external view returns (address);
+    function isSupporter(uint256, address) external view returns (bool);
+    function nextTokenId() external view returns (uint256);
+    function initiativeCount() external view returns (uint256);
+    function incentives() external view returns (IIncentives);
+
+    // Public functions
+    function initialize(SignalsConfig calldata config) external;
+    function proposeInitiative(string memory title, string memory body) external;
+    function proposeInitiativeWithLock(string memory title, string memory body, uint256 amount, uint256 lockDuration) external returns (uint256);
+    function supportInitiative(uint256 initiativeId, uint256 amount, uint256 lockDuration) external returns (uint256);
+    function acceptInitiative(uint256 initiativeId) external payable;
+    function expireInitiative(uint256 initiativeId) external payable;
+    function redeem(uint256 tokenId) external;
+    function getBondInfo(uint256 tokenId) external view returns (IBondIssuer.BondInfo memory);
+    function getTokenMetadata(uint256 tokenId) external view returns (TokenLock memory);
+    function currentDiscount(uint256 tokenId) external view returns (uint256);
+    function getInitiative(uint256 initiativeId) external view returns (Initiative memory);
+    function getSupporters(uint256 initiativeId) external view returns (address[] memory);
+    function getWeight(uint256 initiativeId) external view returns (uint256);
+    function getWeightAt(uint256 initiativeId, uint256 timestamp) external view returns (uint256);
+    function getWeightForSupporterAt(uint256 initiativeId, address supporter, uint256 timestamp) external view returns (uint256);
+    function token() external view returns (address);
+    function totalInitiatives() external view returns (uint256);
+    function totalSupporters(uint256 initiativeId) external view returns (uint256);
+    function setInactivityThreshold(uint256 _newThreshold) external;
+    function setDecayCurve(uint256 _decayCurveType, uint256[] calldata _decayCurveParameters) external;
+    function setIncentives(address _incentives) external;
+    function getPositionsForInitiative(uint256 initiativeId) external view returns (uint256[] memory);
+    function getLockCountForSupporter(address supporter) external view returns (uint256);
+    function getLocksForSupporter(address supporter) external view returns (uint256[] memory);
+    function listPositions(address owner) external view returns (uint256[] memory);
+}
