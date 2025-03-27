8a2369ef83e6fce5bf0bc9bed23d8c54cea4f3e1
x43n <x43n@pm.me>
2025-03-27 14:01:34 +1100

    fix: Render ConnectButton when ready

--- apps/interface/src/components/web3/connect-button.tsx
+++ apps/interface/src/components/web3/connect-button.tsx
@@ -6,7 +6,9 @@ import { Button } from '../ui/button'
 
 // Uses Privy's login/logout functionality
 export const ConnectButton: React.FC = () => {
-  const { login, logout, authenticated } = usePrivy()
+  const { login, logout, authenticated, ready } = usePrivy()
+
+  if (!ready) return null
 
   return (
     <div>
