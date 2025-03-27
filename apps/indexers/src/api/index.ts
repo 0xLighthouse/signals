8a3f85e2a44f96d1545a3844f61fc6090d7e1278
1a35e1 <1a35e1@protonmail.com>
2025-03-27 14:09:35 +0000

    feat: Fetch quotes from API

--- apps/indexers/src/api/index.ts
+++ apps/indexers/src/api/index.ts
@@ -6,6 +6,7 @@ import { getLocks } from './get.locks'
 import { getInitiatives } from './get.initiatives'
 import { getPools } from './get.pools'
 import { getPoolState } from './get.pool-state'
+import { getQuote } from './get.quote'
 
 const app = new Hono()
 
@@ -16,7 +17,7 @@ app.use('/graphql', graphql({ db, schema }))
 
 /**
  * @returns List initiatives for a given chainId and address
- * @example http://localhost:42069/initiatives/421614/0x844c0dd2995cd430aab7ddd1dca3fb15836674bc
+ * @example http://localhost:42069/initiatives/421614/0x7E00a6dfF783649fB3017151652448647600D47E
  */
 app.get('/initiatives/:chainId/:address', getInitiatives)
 
@@ -36,5 +37,12 @@ app.get('/pools/:chainId/:currency', getPools)
  * @returns Pool state for a given poolId
  */
 app.get('/pool-state/:chainId/:poolId', getPoolState)
+
+/**
+ * @returns Quote for a given poolId and tokenId
+ * @example http://localhost:42069/quote/421614/0xA429a75F874B899Ee6b0ea080d7281544506b8c0/1?type=user-buy
+ * @example http://localhost:42069/quote/421614/0xA429a75F874B899Ee6b0ea080d7281544506b8c0/1?type=user-sell
+ */
+app.get('/quote/:chainId/:address/:tokenId', getQuote)
 
 export default app
