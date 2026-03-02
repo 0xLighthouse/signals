c8d5d859384b710e669fa05d0331ca1aa5f0e3a3
1a35e1 <1a35e1@protonmail.com>
2026-03-02 16:32:03 +0000

    feat: Explore fonts

--- apps/interface/src/app/fonts.ts
+++ apps/interface/src/app/fonts.ts
@@ -1,23 +1,5 @@
 import localFont from "next/font/local";
 
-
-export const openSauce = localFont({
-  src: [
-    {
-      path: "../../public/fonts/open-sauce-one-latin-500-normal.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/open-sauce-one-latin-500-italic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-  ],
-  variable: "--font-open-sauce",
-  display: "swap",
-});
-
 export const apercu = localFont({
   src: [
     {
